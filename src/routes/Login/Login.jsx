0858c2f2c5058c429652084d6d04e47b9b9e5a9b
Marius Dima <shady1kx@gmail.com>
2022-11-23 18:49:54 +0200

    fixed leaderboard on leaderboard page

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './login.module.css';
 import bgvid from '../../../src/assets/videos/c-o-animated-ionia.webm';
 import LoginForm from '../../components/Login/LoginForm';
@@ -7,6 +7,18 @@ import Navbar from '../../components/Navbar/Navbar';
 /* import loginBgVideo from '../../../src/assets/videos/animated-bloodmoon-diana.webm.webm'; */
 
 const Login = () => {
+
+
+  const challengerQueueUrl = `https://evening-meadow-61634.herokuapp.com/https://eun1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-490cfa7f-b18a-4d7d-8464-fe5841bc4ff1`
+    
+    useEffect(() => {
+        fetch(challengerQueueUrl)
+        .then(response => response.json())
+        .then(data => console.log(data.entries));
+  
+      },[])
+
+
   return (
     <div className={style['login-container']}>
         <video className={style['bg-video']} autoPlay loop muted playsInline>
