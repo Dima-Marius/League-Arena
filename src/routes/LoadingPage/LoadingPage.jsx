46fd13e71c84eaa79d5f75a4f04d4f6227c1d6cc
Marius Dima <shady1kx@gmail.com>
2022-12-06 23:46:47 +0200

    init user profiles

---
+++ src/routes/LoadingPage/LoadingPage.jsx
@@ -0,0 +1,28 @@
+import Footer from '../../components/Footer/Footer';
+import Navbar from '../../components/Navbar/Navbar';
+import style from './loadingPage.module.css'
+
+
+const LoadingPage = () => {
+  return (
+ <div className={style.container}>
+    <div className={style.nav}>
+      <Navbar/>
+    </div>
+    <div className={style['loading-container']}>
+      <div>
+        <div className={style.loader}>
+        <div className={style.inner}>
+        </div>
+        </div>
+       <p className={style['website-title']}>League Arena <i className="fa-solid fa-chess-knight"></i></p>
+      </div>
+    </div>
+    <div className={style.footer}>
+      <Footer/>
+      </div>
+ </div>
+  )
+}
+
+export default LoadingPage
