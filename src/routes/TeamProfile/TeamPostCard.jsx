2780601c61a02e4d0078262797eef052462b0abe
Marius Dima <shady1kx@gmail.com>
2022-12-24 20:51:34 +0200

    team profile minor changes

--- src/routes/TeamProfile/TeamPostCard.jsx
+++ src/routes/TeamProfile/TeamPostCard.jsx
@@ -2,25 +2,46 @@ import React from 'react';
 import { useContext } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { AiOutlineLike } from 'react-icons/ai'
+import { AiFillLike } from 'react-icons/ai'
 import ApiKeyContext from '../../context/ApiKeyContext';
 import style from './teamPostCard.module.css';
-import { AiOutlineLike } from 'react-icons/ai'
 
 const TeamPostCard = (props) => {
-    const { title, description, author } = props;
+    const { title, content, author, date, time, likes, setTeamData, comments, teamData, likeUpdate} = props;
     const API_KEY_CTX = useContext(ApiKeyContext) 
     const [authorIcon, setAuthorIcon] = useState(JSON.parse(localStorage.getItem(author)) ?? 7)
+    const [like, setLike] = useState(false)
+    const [commentCount, setCommentCount] = useState(comments.length ?? 0)
+    const [likeCount, setLikeCount] = useState(likes.length)
  
     const userProfileUrl = `https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${author}?api_key=${API_KEY_CTX.apiKey}`
     const profilePictureUrl = `http://ddragon.leagueoflegends.com/cdn/12.23.1/img/profileicon/${JSON.parse(localStorage.getItem(author)) ?? authorIcon}.png`
-  
-    const currentDate = new Date();
+
+    const addLike = {discussions: [...teamData.discussions]}
+    const removeLike = {likes: likes.filter(like => like !== author)}
+
+    const likeHandler = () => {
+        setLike(!like);
+        setTeamData({...teamData, discussions: [...teamData.discussions, ]})
+         fetch(`http://localhost:3500/createdTeams/${teamData.id}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(like ? addLike : removeLike)
+        })
+        console.log(title,content,author,date,time)
+        likeUpdate(!like);
+    }
 
      useEffect(() => {
+      if (!localStorage.getItem(author)) {
             fetch(userProfileUrl)
             .then(res => res.json())
             .then(data => localStorage.setItem(author,JSON.stringify(data.profileIconId)))
             .finally(() => setAuthorIcon(JSON.parse(localStorage.getItem(author))));
+          }
     },[author,userProfileUrl,authorIcon.profileIconId,authorIcon])
 
   return (
@@ -35,31 +56,39 @@ const TeamPostCard = (props) => {
             </p>
           </div>
             <p>
-              <i className={style.date}>Posted on {currentDate.toLocaleDateString()}, {currentDate.toLocaleTimeString()}</i>
+              <i className={style.date}>Posted on {date}, {time}</i>
             </p>
+            { 
+            <p className={style.delete}>
+              <button className={style['delete-btn']}>
+              <i className="fa-solid fa-trash-can"></i>
+              </button>
+            </p>
+            }
         </div>
         <div className={style.data}>
           <p className={style.title}>
             {title}
             </p>
-          <p className={style.description}>{description} Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis quo fugit consequuntur quam magnam dignissimos praesentium neque velit aliquid incidunt dolor, repellendus porro illum libero delectus soluta! Ducimus quae voluptatibus quibusdam commodi libero id debitis dignissimos tempore expedita nam mollitia eaque facilis, explicabo nulla perferendis qui autem. Numquam, deserunt facere.</p>
+          <p className={style.description}>{content}</p>
         </div>
         <div className={style['post-footer']}>
-          <p>Liked by <span>PSG Shady</span></p>
+          <ul className={style.likes}>{likes.length !== 0 && <p>Liked by</p>} {likes.length !== 0 ? likes.map(item => <li>{item}</li>) : 'No likes yet'}</ul>
         </div>
         <ul className={style.buttons}>
-          <li className={style['like-btn']}>
-            <button>
-              <AiOutlineLike size='25'/>
+          <li className={style.like}>
+            <button className={style['like-btn']} onClick={likeHandler}>
+              {!like ? <AiOutlineLike size='25'/> : <AiFillLike size='25'/>}
               <span>Like</span>
             </button>
           </li>
           <li>
-            <button>
+            <button className={style['comment-btn']}>
               <i className="fa-regular fa-comment"></i>
               <span>Comment</span>
             </button>
           </li>
+          <span className={style.comments}>{commentCount} comments</span>
         </ul>
     </div>
   )
